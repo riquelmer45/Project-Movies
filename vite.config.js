bd4c56c94cb9fcfc31e1170e798d1906f1f67e34
riquelmer45 <riquelmer_modesto@hotmail.com>
2024-07-08 21:38:25 -0300

    Update GitPages config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import replace from "@rollup/plugin-replace"; // Importe o plugin
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    replace({
+      // Adicione o plugin replace na lista de plugins
+      preventAssignment: true,
+      "http://localhost:5173/": "https://riquelmer45.github.io/Project-Movies/",
+      __buildDate__: () => JSON.stringify(new Date()),
+      __buildVersion__: 15,
+    }),
+  ],
   base: "/Project-Movies/",
-    build: {
-    outDir: 'docs',
+  build: {
+    outDir: "docs",
   },
 });
